0e59c8cf8787911afbf375dec3eee34f61cb604f
Pedro Coder <kfidelak@gmail.com>
2022-09-20 20:20:56 +0000

    Add SKU to Product Card LWC

--- force-app/main/default/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/productCard/productCard.js
@@ -22,6 +22,7 @@ import MATERIAL_FIELD from '@salesforce/schema/Product__c.Material__c';
 import FOPK_FIELD from '@salesforce/schema/Product__c.Fork__c';
 import FRONT_BRAKES_FIELD from '@salesforce/schema/Product__c.Front_Brakes__c';
 import REAR_BRAKES_FIELD from '@salesforce/schema/Product__c.Rear_Brakes__c';
+import SKU_FIELD from '@salesforce/schema/Product__c.SKU__c';
 
 /**
  * Component to display details of a Product__c.
@@ -38,6 +39,7 @@ export default class ProductCard extends NavigationMixin(LightningElement) {
     forkField = FOPK_FIELD;
     frontBrakesField = FRONT_BRAKES_FIELD;
     rearBrakesField = REAR_BRAKES_FIELD;
+    skuField = SKU_FIELD;
 
     // Id of Product__c to display
     recordId;
